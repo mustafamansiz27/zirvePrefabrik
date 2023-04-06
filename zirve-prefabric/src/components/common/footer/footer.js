c674e7630296861f13e02fbb50b66f39d111ee7b
mustafa <m.gonulgul49@gmail.com>
2023-04-06 06:18:59 +0300

    v1 06.04

--- zirve-prefabric/src/components/common/footer/footer.js
+++ zirve-prefabric/src/components/common/footer/footer.js
@@ -8,12 +8,12 @@ const Footer = () => {
     <div className='footer'>
         <Container>
         <Row>
-            <Col lg={3}> <img src={logoFooter} alt="footer logo" className='img-fluid' /></Col>
-            <Col lg={3}>
+            <Col lg={3} md={6}> <img src={logoFooter} alt="footer logo" className='img-fluid' /></Col>
+            <Col lg={3} md={6}>
                 <h2>Kimiz Biz</h2>
                 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nihil possimus magni voluptates a vero architecto sed alias omnis ullam, ipsam fugit ipsum aut, odit laboriosam eaque voluptatum nesciunt repellendus? </p>
             </Col>
-            <Col lg={3}> 
+            <Col lg={3} md={6}> 
                <h2>İletişim</h2>
                <ul>
                 <li>1</li>
@@ -22,7 +22,7 @@ const Footer = () => {
                 <li>4</li>
                </ul>
             </Col>
-            <Col lg={3}>
+            <Col lg={3} md={6}>
                  <h2>Adresimiz</h2>
                  <iframe src="" frameborder="0"></iframe>
             </Col>
