e7b3d9c74c46caa859270c6df436712624e3e298
mustafa <m.gonulgul49@gmail.com>
2023-04-07 23:01:17 +0300

    v23

--- zirve-prefabric/src/components/common/footer/footer.js
+++ zirve-prefabric/src/components/common/footer/footer.js
@@ -2,6 +2,8 @@ import React from 'react'
 import "./footer.scss"
 import logoFooter from "../../../assets/img/product/footerLogoSiyah.jpg"
 import { Col, Container, Row } from 'react-bootstrap'
+import {BsFillTelephoneFill, BsTwitter} from 'react-icons/bs'
+import { AiFillInstagram, AiOutlineMail } from 'react-icons/ai'
 
 const Footer = () => {
   return (
@@ -16,14 +18,15 @@ const Footer = () => {
             <Col lg={3} md={6}> 
                <h2>İletişim</h2>
                <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
+               <a href="tel:0554 138 50 12"> <li><BsFillTelephoneFill/> 0554 138 50 12 </li> </a>
+               <a href="https://instagram.com/ilhanmansiz41?igshid=OGRjNzg3M2Y=">  <li><AiFillInstagram/>  </li></a>
+                <li><BsTwitter/></li>
+                <li><AiOutlineMail/></li>
                </ul>
             </Col>
             <Col lg={3} md={6}>
                  <h2>Adresimiz</h2>
+                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate quaerat, quas, placeat ad architecto at quidem porro earum odit, officiis ut necessitatibus dolore libero. Sit suscipit vitae quis ducimus.</p>
                  <iframe src="" frameborder="0"></iframe>
             </Col>
         </Row>
